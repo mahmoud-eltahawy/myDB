df148f0f5d37fe45ffae3c7b80fc5af9faad28cf
mahmoud-eltahawy <mahmoudeltahawy559@gmail.com>
2022-07-17 05:43:40 +0200

    enough security

--- src/main/java/com/db/tahawy/security/AppUserRole.java
+++ src/main/java/com/db/tahawy/security/AppUserRole.java
@@ -1,25 +1,8 @@
 package com.db.tahawy.security;
 
-import java.util.Set;
-
-import static com.db.tahawy.security.AppUserPermission.*;
-
-import com.google.common.collect.Sets;
 
 public enum AppUserRole {
-	USER(Sets.newHashSet(FILE_UPLOAD,FILE_PLACE,FILE_DOWNLOAD,
-			FILE_PUBLISH,FILE_SHARE,FILE_DELETE,USER_IN)),
-	SUPER_USER(Sets.newHashSet(FILE_UPLOAD,FILE_PLACE,FILE_DOWNLOAD,
-			FILE_PUBLISH,FILE_SHARE,FILE_DELETE,USER_IN)),
-	ADMIN(Sets.newHashSet(USER_LIST,TYPE_LIST,TYPE_FILES,ROOT_SET));
-	
-	private final Set<AppUserPermission> permissions;
-
-	AppUserRole(Set<AppUserPermission> permissions) {
-		this.permissions = permissions;
-	}
-	
-	public Set<AppUserPermission> getPermissions(){
-		return this.permissions;
-	}
+	USER,
+	SUPER_USER,
+	ADMIN
 }
